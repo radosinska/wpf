7e3bfc2ae12beca04c3ea3117b71f53bf8c9d15b
radosinska <radosinska@wp.pl>
2015-02-28 12:00:14 +0100

    Add TwoInt class

---
+++ NewtonsCradle/TwoInt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonsCradle
+{
+    public class TwoInt
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        
+        public TwoInt(int a,int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+}
